7795865e4b8ce758dd06c377e86cdc6bb00fcdad
yarinmatmoni <yarinmatmoni@gmail.com>
2023-02-05 14:54:37 +0200

    test server deploy

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,47 @@
-require("dotenv").config();
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const port = process.env.PORT;
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
+const http = require('http');
+const { Server } = require('socket.io');
 const server = http.createServer(app);
-const initialDB = require("./db");
+const initialDB = require('./db');
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET"],
-  },
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET'],
+	},
 });
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "1mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
 app.use(bodyParser.json());
 
 initialDB();
 
-const codeBlockRouter = require("./routes/codeBlockRoutes");
-app.use("/codeBlock", codeBlockRouter);
+app.get('/test', (req, res) => {
+	res.send('deploy server');
+});
+
+const codeBlockRouter = require('./routes/codeBlockRoutes');
+app.use('/codeBlock', codeBlockRouter);
 
 let numOfClients = 0;
 
-io.on("connection", (socket) => {
-  numOfClients += 1;
-  socket.emit("clientsCounter", numOfClients);
+io.on('connection', (socket) => {
+	numOfClients += 1;
+	socket.emit('clientsCounter', numOfClients);
 
-  socket.on("disconnect", () => {
-    numOfClients -= 1;
-  });
+	socket.on('disconnect', () => {
+		numOfClients -= 1;
+	});
 
-  socket.on("codeChange", (data) => {
-    io.emit("sendEditCode", data.data);
-  });
+	socket.on('codeChange', (data) => {
+		io.emit('sendEditCode', data.data);
+	});
 });
 
 server.listen(port, () => {
-  console.log("Server is running on port " + port);
+	console.log('Server is running on port ' + port);
 });
