88d2bad781b88624b21db5d210ec216902b9d57d
yarinmatmoni <yarinmatmoni@gmail.com>
2023-02-06 01:36:28 +0200

    getServerUrl

--- frontend/src/common/function.ts
+++ frontend/src/common/function.ts
@@ -1,6 +1,6 @@
 export const getServerUrl = () => {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://moveobackend.vercel.app';
+    return 'https://moveoprojectfronte.onrender.com';
   } else {
     return 'http://localhost:4000';
   }
