cfd23ed6351e5d7e010c586a40e200782104ad41
yarinmatmoni <yarinmatmoni@gmail.com>
2023-02-06 18:07:13 +0200

    refactor code

--- frontend/src/pages/codeBlocks/[id].tsx
+++ frontend/src/pages/codeBlocks/[id].tsx
@@ -24,7 +24,7 @@ export async function getServerSideProps(context: any) {
 
 function CodeBlockPage({ codeBlock }: any) {
   const [code, setCode] = useState(codeBlock.code);
-  const [users, setUsers] = useState<number>();
+  const [users, setUsers] = useState<number>(0);
   const socketUrl = getServerUrl();
 
   const [socket, setSocket] = useState(io(`${socketUrl}`, { transports: ['websocket'] }));
@@ -41,52 +41,34 @@ function CodeBlockPage({ codeBlock }: any) {
   });
 
   socket.on('sendEditCode', (data: string) => {
-    // setCode(data);
-    // console.log(data);
     if (users !== 1) {
       setCode(data);
     }
   });
 
-
-  // useEffect(() => {
-  //   const socketUrl = getServerUrl();
-  //   const socket = io(`${socketUrl}`, { transports: ['websocket'] });
-
-  //   socket.on('clientsCounter', (data: number) => {
-  //     setUsers(data);
-  //   });
-
-  //   socket.on('sendEditCode', (data: string) => {
-  //     setCode(data);
-  //   });
-
-  //   return () => {
-  //     socket.disconnect();
-  //   };
-  // }, []);
-
   const handleCodeChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    // setCode(e.target.value);
     socket.emit('codeChange', { data: e.target.value });
   };
 
 
   return (
     <div className={style.codeBlockPage}>
-      <Header
-        tabName={codeBlock.title}
-        title={codeBlock.title}
-        subTitle={`${users === 1 ? 'Mentor' : 'Student'} Panel`}
-      ></Header>
-      <CodeEditor
-        value={code}
-        language="js"
-        onChange={handleCodeChange}
-        padding={25}
-        className={style.codeEditor}
-        readOnly={users === 1}
-      />
+      {users !== 0 && <>
+        <Header
+          tabName={codeBlock.title}
+          title={codeBlock.title}
+          subTitle={`${users === 1 ? 'Mentor' : 'Student'} Panel`}
+        ></Header>
+        <CodeEditor
+          value={code}
+          language="js"
+          onChange={handleCodeChange}
+          padding={25}
+          className={style.codeEditor}
+          readOnly={users === 1}
+        />
+      </>
+      }
     </div>
   );
 }
