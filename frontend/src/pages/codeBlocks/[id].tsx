1364abe37e3689dc1119d3dba5c7c43447793c07
yarinmatmoni <yarinmatmoni@gmail.com>
2023-02-06 16:44:02 +0200

    refactor code

--- frontend/src/pages/codeBlocks/[id].tsx
+++ frontend/src/pages/codeBlocks/[id].tsx
@@ -47,7 +47,11 @@ function CodeBlockPage({ codeBlock }: any) {
     const socketUrl = getServerUrl();
     const socket = io(`${socketUrl}`, { transports: ['websocket'] });
 
-    socket.emit('codeChange', { data: e.target.value });
+    if (socket) {
+      const newCode = e.target.value;
+      setCode(newCode);
+      socket.emit('codeChange', { data: e.target.value });
+    }
   };
 
   return (
