35ff3582eb9b20561f7c088ff0098e843bb61b36
yarinmatmoni <yarinmatmoni@gmail.com>
2023-02-06 16:38:03 +0200

    refactor code

--- frontend/src/pages/codeBlocks/[id].tsx
+++ frontend/src/pages/codeBlocks/[id].tsx
@@ -28,14 +28,14 @@ function CodeBlockPage({ codeBlock }: any) {
 
   useEffect(() => {
     const socketUrl = getServerUrl();
-    const socket = io(`${socketUrl}`, { upgrade: false, transports: ['websocket'] });
+    const socket = io(`${socketUrl}`, { transports: ['websocket'] });
 
-    socket.on('clientsCounter', (data) => {
+    socket.on('clientsCounter', (data: number) => {
       setUsers(data);
     });
 
-    socket.on('sendEditCode', (data) => {
-      setCode(data)
+    socket.on('sendEditCode', (data: string) => {
+      setCode(data);
     });
 
     return () => {
@@ -44,12 +44,10 @@ function CodeBlockPage({ codeBlock }: any) {
   }, []);
 
   const handleCodeChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const newCode = e.target.value;
-    setCode(newCode);
     const socketUrl = getServerUrl();
-    const socket = io(`${socketUrl}`, { upgrade: false, transports: ['websocket'] });
+    const socket = io(`${socketUrl}`, { transports: ['websocket'] });
 
-    socket.emit("codeChange", newCode);
+    socket.emit('codeChange', { data: e.target.value });
   };
 
   return (
